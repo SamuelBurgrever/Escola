e545d5ec01eed8f3578e6529aec13370873deeab
Samuel Burgrever Torres <samuel.burgrever@outlook.com>
2024-07-17 15:48:30 -0300

    Separei a inicialização dos contextos

--- src/main/java/br/com/alura/escola/gamificacao/infra/selo/RepositorioDeSelosEmMemoria.java
+++ src/main/java/br/com/alura/escola/gamificacao/infra/selo/RepositorioDeSelosEmMemoria.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import br.com.alura.escola.academico.dominio.aluno.CPF;
 import br.com.alura.escola.gamificacao.dominio.selo.RepositorioDeSelos;
 import br.com.alura.escola.gamificacao.dominio.selo.Selo;
+import br.com.alura.escola.shared.dominio.CPF;
 
 public class RepositorioDeSelosEmMemoria implements RepositorioDeSelos {
 
-	private List<Selo> selos = new ArrayList<>();
+    private List<Selo> selos = new ArrayList<>();
 
-	@Override
-	public void adicionar(Selo selo) {
-		this.selos.add(selo);
-	}
+    @Override
+    public void adicionar(Selo selo) {
+        this.selos.add(selo);
+    }
 
-	@Override
-	public List<Selo> selosDoAlunoDeCPF(CPF cpf) {
-		return this.selos
-				.stream()
-				.filter(s -> s.getCpfDoAluno().equals(cpf))
-				.collect(Collectors.toList());
-	}
+    @Override
+    public List<Selo> selosDoAlunoDeCPF(CPF cpf) {
+        return this.selos
+                .stream()
+                .filter(s -> s.getCpfDoAluno().equals(cpf))
+                .collect(Collectors.toList());
+    }
 
 }
